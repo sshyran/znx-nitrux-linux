5708e6b5cf50780ef982def21fac0e42c33d5b93
Luis Lavaire <llavaire01@gmail.com>
2018-07-15 14:59:42 -0600

    Code cleanup. Use the '-Z' (clear GPT and MBR data) instead of '-z' on the 'sgdisk' command.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 apt-get update
 apt-get install -qy wget patchelf file
-apt-get install -qy gdisk zsync util-linux btrfs-tools dosfstools grub-common grub-efi-amd64-bin
+apt-get install -qy gdisk zsync util-linux btrfs-tools dosfstools grub-common grub2-common grub-efi-amd64-bin
 
 wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 wget -q https://raw.githubusercontent.com/luis-lavaire/bin/master/copier
@@ -44,6 +44,11 @@ $APPDIR/bin/znx $@' > appdir/bin/wrapper
 
 chmod a+x appdir/bin/wrapper
 
+
+# -- Install busybox.
+
+./copier busybox appdir
+/bin/busybox --install -s appdir/bin
 
 # -- Copy binaries and its dependencies to appdir.
 
