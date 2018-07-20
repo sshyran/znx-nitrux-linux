815adf016ba32d1b41806feeca02db66d630d0cf
Luis Lavaire <llavaire01@gmail.com>
2018-07-20 14:10:32 -0600

    Persistence works as spected.

--- persistence.sh
+++ persistence.sh
@@ -1,24 +1,26 @@
-#! /bin/sh
+#! /bin/sh -x
 
 # This setup assumes that ONLY ONE device is connected. To support
 # data persistence correctly, the filesystem UUID should be passed
 # as a kernel parameter.
 
-# -- Prepare the temporary directory
+# -- Storage location.
+
+STORAGE=$HOME/storage
 
 for d in $(awk '{ print $4 }' /proc/partitions); do
 
 	[ -b /dev/$d ] || continue
 
+
 	blkid /dev/$d | grep -q ZNX_DATA && {
 		p=/dev/$d
-		s=$(grep $p /proc/mounts | awk '{ print $2 }')
 
-		mkdir $HOME/storage
-		chown $(id -un) $HOME/storage
-		chmod 700 $HOME/storage
-		sudo mount $s $HOME/storage
+		mkdir -p $STORAGE
+		chown $(id -un) $STORAGE
+		chmod 700 $STORAGE
 
+		sudo mount -o bind $(grep $p /proc/mounts | cut -d ' ' -f 2) $STORAGE
 		break
 	}
 
