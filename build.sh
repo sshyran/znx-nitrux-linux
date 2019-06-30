3eeb54a1f033543e66157760e9cdd2f41c3ca9c3
Uri <hello@uriherrera.com>
2019-06-30 07:42:54 -0500

    btrfs-progs doesn't exist in xenial the correct name is btrfs-tools

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ TRAVIS_BRANCH=$2
 
 apt-get -qq -y update > /dev/null
 apt-get -qq -y install wget patchelf file libcairo2 > /dev/null
-apt-get -qq -y install xorriso axel gdisk zsync btrfs-progs dosfstools grub-common grub2-common grub-efi-amd64 grub-efi-amd64-bin > /dev/null
+apt-get -qq -y install xorriso axel gdisk zsync btrfs-tools dosfstools grub-common grub2-common grub-efi-amd64 grub-efi-amd64-bin > /dev/null
 apt-get -qq -y install git autoconf gettext automake libtool-bin autopoint pkg-config libncurses5-dev bison > /dev/null
 
 wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
