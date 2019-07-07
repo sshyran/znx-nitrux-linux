a1d93c89a25ccd6bbbcf8fe2b16751995a378367
Uri Herrera <hello@uriherrera.com>
2019-07-06 19:35:49 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -41,6 +41,7 @@ chmod +x appdir/znx
 
  # Remove old libsmartcols libraries for lsblk to find the correct one	
 rm /lib/x86_64-linux-gnu/libsmartcols.so.1*
+rm /lib/x86_64-linux-gnu/libmount.so.1*
 
 # -- Write the commit that generated this build.
 
