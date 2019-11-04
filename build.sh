078e739ba6566fd7ce1409dfa814550be1572c88
luis-lavaire <llavaire01@gmail.com>
2019-11-04 16:48:33 -0600

    Update build.sh to the latest tools.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ apt -qq update > /dev/null
 apt -yy install wget patchelf file libcairo2 mtools xorriso axel gdisk zsync btrfs-progs dosfstools grub-common grub2-common grub-efi-amd64 grub-efi-amd64-bin git autoconf gettext automake libtool-bin autopoint pkg-config libncurses5-dev bison
 
 
-# -- Update xorriso, grub, util-linux.
+#    Update xorriso, grub, util-linux.
 
 files='
 http://mirrors.kernel.org/ubuntu/pool/universe/libi/libisoburn/xorriso_1.5.0-1build1_amd64.deb
@@ -45,7 +45,6 @@ wget -q https://raw.githubusercontent.com/Nitrux/tools/master/copier
 chmod +x appimagetool
 chmod +x copier
 chmod +x execs
-chmod +x appdir/znx
 
 
 #    Build util-linux 2.34.
@@ -74,7 +73,6 @@ rm /lib/x86_64-linux-gnu/libmount.so.1*
 wget -q https://raw.githubusercontent.com/Nitrux/tools/master/mkiso -o /bin/mkiso
 chmod +x /bin/mkiso
 
-
 ./copier appdir $(./execs appdir/znx)
 
 
@@ -92,7 +90,7 @@ grub-mkimage \
 	echo read ls cat png jpeg halt reboot
 
 
-#   Variables for generating the AppImage.
+#    Variables for generating the AppImage.
 
 ARCH=$(uname -m)
 TRAVIS_COMMIT=${1:0:7}
@@ -107,7 +105,7 @@ UPDATE_URL="zsync|https://github.com/Nitrux/znx/releases/download/continuous-$TR
 sed -i "s/@TRAVIS_COMMIT@/$TRAVIS_COMMIT/" appdir/znx
 
 
-#   Generate the AppImage.
+#    Generate the AppImage.
 
 (
 	cd appdir
@@ -125,8 +123,8 @@ sed -i "s/@TRAVIS_COMMIT@/$TRAVIS_COMMIT/" appdir/znx
 	find usr/sbin -type f -exec patchelf --set-rpath '$ORIGIN/../../lib/x86_64-linux-gnu' {} \;
 )
 
-wget -q https://raw.githubusercontent.com/Nitrux/appimage-wrapper/master/appimage-wrapper
-chmod a+x appimage-wrapper
+wget -q https://raw.githubusercontent.com/Nitrux/tools/master/aw
+chmod a+x aw
 
 mkdir out
-./appimage-wrapper appimagetool -u "$UPDATE_URL" appdir out/$RELEASE_NAME
+./aw appimagetool -u "$UPDATE_URL" appdir out/$RELEASE_NAME
